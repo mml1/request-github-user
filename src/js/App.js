5b2a20cc7825aa70435088babb36fbac16e8265c
Mari Lliguicota <marilliguicota@gmail.com>
2017-12-04 17:54:38 -0800

    reordering files

--- src/js/App.js
+++ src/js/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import SearchBox from './searchBox';
+import SearchBox from './js/searchBox';
 import './App.css';
 
 class App extends Component {
--- src/js/searchBox.js
+++ src/js/searchBox.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Button from './button';
+import Button from './js/button';
 
 class SearchBox extends Component {
   render() {
